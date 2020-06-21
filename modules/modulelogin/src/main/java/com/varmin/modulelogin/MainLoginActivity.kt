ad9f8b8f6975932f5a0e0df99da5e0bafa63757f
yang <varmin@163.com>
2020-06-22 00:17:49 +0800

    module/lib

---
+++ modules/modulelogin/src/main/java/com/varmin/modulelogin/MainLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.varmin.modulelogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_login)
+    }
+}
