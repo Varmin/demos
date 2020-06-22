b34e77da47c9b7490e475c045de761f8c461945e
yang <varmin@163.com>
2020-06-22 22:31:51 +0800

    modules 搭建

---
+++ modules/modulechat/src/main/java/com/varmin/modulechat/MainChatActivity.kt
@@ -0,0 +1,12 @@
+package com.varmin.modulechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_chat)
+    }
+}
