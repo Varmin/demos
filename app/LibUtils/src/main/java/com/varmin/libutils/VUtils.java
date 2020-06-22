d875cd61f3967102fae09e3198f54126e1811184
yang <varmin@163.com>
2020-06-22 23:27:11 +0800

    LibUtils

---
+++ app/LibUtils/src/main/java/com/varmin/libutils/VUtils.java
@@ -0,0 +1,25 @@
+package com.varmin.libutils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Created by HuangYang
+ * on 2019-05-28  22:59.
+ * 文件描述：常用工具类
+ */
+public class VUtils {
+    /**
+     * dp转px
+     */
+    public static int dp2px(Context context, float dpVal){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dpVal,context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp转px
+     */
+    public static int sp2px(Context context, float spVal){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,spVal,context.getResources().getDisplayMetrics());
+    }
+}
