8b4fdd501dd3da4683f74dbe799a3d7b762eb75c
yang <varmin@163.com>
2020-07-29 18:35:58 +0800

    。。。

--- app/src/main/java/com/varmin/demos/MainActivityApp.kt
+++ app/src/main/java/com/varmin/demos/MainActivityApp.kt
@@ -7,5 +7,7 @@ class MainActivityApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- modules/modulemain/src/main/java/com/varmin/modulemain/MainActivityMain.kt
+++ modules/modulemain/src/main/java/com/varmin/modulemain/MainActivityMain.kt
@@ -1,6 +1,8 @@
 package com.varmin.modulemain
 
+import com.bumptech.glide.Glide
 import com.varmin.modulebase.BaseActivity
+import kotlinx.android.synthetic.main.activity_main_main.*
 
 class MainActivityMain : BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_main_main
@@ -9,6 +11,12 @@ class MainActivityMain : BaseActivity() {
     }
 
     override fun initData() {
+        Glide.with(this)
+//            .asGif()
+//            .error(R.mipmap.ic_launcher)
+            .load(R.mipmap.icon_redpacket)
+//            .load(resources.getDrawable(R.mipmap.icon_redpacket))
+            .into(ivTest)
     }
 
 }
