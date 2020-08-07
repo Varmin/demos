724a1c9c4f9dd6f390b5445243b3acaf0b68e78e
Varmin <varmin@163.com>
2020-08-07 10:47:24 +0800

    算法：第一个出现的字符

--- project/src/main/java/com/varmin/project/algorithm/CharRealm.java
+++ project/src/main/java/com/varmin/project/algorithm/CharRealm.java
@@ -4,6 +4,9 @@ import com.varmin.project.base.BaseImpl;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 /**
  * author：yang
@@ -16,7 +19,24 @@ public class CharRealm extends BaseImpl {
         // 反转字符串
         char[] reversArr = new char[]{'V', 'a', 'r', 'm', 'i', 'n'};
         reversChar(reversArr);
+        // 字符串中的第一个唯一字符
+        String str = "hahahahello";
+        firstSingleChar(str);
+    }
 
+    private int firstSingleChar(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.charAt(i)) == 1) {
+                System.out.println("CharRealm.firstSingleChar: index="+i+", char="+ str.charAt(i));
+                return i;
+            }
+        }
+        return -1;
     }
 
     //--------------------------反转字符串--------------------------
